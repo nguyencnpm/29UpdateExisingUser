0e2b3be5ece39ec735d6fb60892df41d6047b65f
binhnguyen <nguyencnpm@gmail.com>
2020-09-30 06:20:22 +0700

    Alert message using TempData

--- eShopSolution.AdminApp/Controllers/UserController.cs
+++ eShopSolution.AdminApp/Controllers/UserController.cs
@@ -28,9 +28,14 @@ namespace eShopSolution.AdminApp.Controllers
                 PageSize = pageSize
             };
 
-            ViewBag.keyWord = keyWord;
-
             var data = await _userApiClient.GetUsersPagings(request);
+
+            ViewBag.KeyWord = keyWord;
+
+            if (TempData["result"] != null)
+            {
+                ViewBag.SuccessMsg = TempData["result"];
+            }
 
             return View(data.ResultObj);
         }
@@ -59,6 +64,7 @@ namespace eShopSolution.AdminApp.Controllers
             var result = await _userApiClient.RegisterUser(request);
             if (result.IsSuccessed)
             {
+                TempData["result"] = "Thêm mới người dùng thành công";
                 return RedirectToAction("Index");
             }
             ModelState.AddModelError("", result.Message);
@@ -97,6 +103,7 @@ namespace eShopSolution.AdminApp.Controllers
             var result = await _userApiClient.UpdateUser(request.Id, request);
             if (result.IsSuccessed)
             {
+                TempData["result"] = "Cập nhật người dùng thành công";
                 return RedirectToAction("Index");
             }
             ModelState.AddModelError("", result.Message);
@@ -129,6 +136,7 @@ namespace eShopSolution.AdminApp.Controllers
             var result = await _userApiClient.Delete(request.Id);
             if (result.IsSuccessed)
             {
+                TempData["result"] = "Xóa người dùng thành công";
                 return RedirectToAction("Index");
             }
             ModelState.AddModelError("", result.Message);
