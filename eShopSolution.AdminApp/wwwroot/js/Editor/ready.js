96d137efd3061325e21877e5e4402f65c454db16
binhnguyen <nguyencnpm@gmail.com>
2020-11-10 23:06:25 +0700

    38 Integrage CK Editor

---
+++ eShopSolution.AdminApp/wwwroot/js/Editor/ready.js
@@ -0,0 +1,10 @@
+﻿ClassicEditor
+	.create(document.querySelector('#editor-details'), {
+		// toolbar: [ 'heading', '|', 'bold', 'italic', 'link' ]
+	})
+	.then(editor => {
+		window.editor = editor;
+	})
+	.catch(err => {
+		console.error(err.stack);
+	});
