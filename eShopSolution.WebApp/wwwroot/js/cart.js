80a49ea49d1b0c2de58dcc64d9cd66a013dd0de0
binhnguyen <nguyencnpm@gmail.com>
2021-01-20 17:32:03 +0700

    53UpdateCart

--- eShopSolution.WebApp/wwwroot/js/cart.js
+++ eShopSolution.WebApp/wwwroot/js/cart.js
@@ -2,6 +2,7 @@
 
     this.initialize = function () {
         LoadData();
+        regsiterEvents();
     }
 
     function LoadData() {
@@ -12,6 +13,9 @@
             dataType: 'json',
             url: '/' + culture + '/cart/GetListItems',
             success: function (res) {
+                if (res.length === 0) {
+                    $('#tbl_cart').hide();
+                }
                 var resHtml = '';
                 var total = 0;
                 $.each(res, function (i, item) {
@@ -22,10 +26,10 @@
                         + " <td>" + item.description +"</td>"
                         + " <td>"
                         + " <div class=\"input-append\">"
-                        + " <input class=\"span1\" style=\"max - width: 34px\" placeholder=\"1\" id=\"appendedInputButtons\" size=\"16\" type=\"text\">"
-                        + " <button class=\"btn\" type = \"button\" > <i class=\"icon-minus\"></i></button >"
-                        + " <button class=\"btn\" type=\"button\"><i class=\"icon-plus\"></i></button>"
-                        + " <button class=\"btn btn - danger\" type=\"button\"><i class=\"icon-remove icon-white\"></i></button>"
+                        + " <input id=\"txt_quantity_" + item.productId + "\" class=\"span1\" placeholder=\"1\" value=\"" + item.quantity + "\" size=\"16\" type=\"text\" style=\"max-width: 34px\">"
+                        + " <button class=\"btn btn_minus\" data-id=\"" + item.productId +"\" type = \"button\" > <i class=\"icon-minus\"></i></button >"
+                        + " <button class=\"btn btn_plus\" data-id=\"" + item.productId +"\" type=\"button\"><i class=\"icon-plus\"></i></button>"
+                        + " <button class=\"btn btn-danger btn_remove\" data-id=\"" + item.productId +"\" type=\"button\"><i class=\"icon-remove icon-white\"></i></button>"
                         + " </div >"
                         + " </td>"
                         + " <td>" + numberWithCommas(item.price) +"</td>"
@@ -44,6 +48,49 @@
             error: function (err) {
                 console.log(err);
             }
+        });
+    }
+
+    function updateCart(id, quantity) {
+        const culture = $("#hidCulture").val();
+        $.ajax({
+            type: "POST",
+            dataType: 'json',
+            url: '/' + culture + '/cart/UpdateCart',
+            data: {
+                id: id,
+                quantity: quantity
+            },
+            success: function (res) {
+                console.log(res);
+                LoadData();
+            },
+            error: function (err) {
+                console.log(err);
+            }
+        });
+    }
+
+    function regsiterEvents() {
+        $('body').on('click', '.btn_plus', function (e) {
+            e.preventDefault();
+            const id = $(this).data('id');
+            const quantity = parseInt($("#txt_quantity_" + id).val()) + 1;
+            updateCart(id, quantity);
+        });
+
+        $('body').on('click', '.btn_minus', function (e) {
+            e.preventDefault();
+            const id = $(this).data('id');
+            const quantity = parseInt($("#txt_quantity_" + id).val()) - 1;
+            updateCart(id, quantity);
+        });
+
+        $('body').on('click', '.btn_remove', function (e) {
+            e.preventDefault();
+            const id = $(this).data('id');
+            const quantity = 0;
+            updateCart(id, quantity);
         });
     }
 }
